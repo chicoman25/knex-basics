4716e42b251e822d6e594fa596999a87a11e42c1
Ryan Chichirico <ryan.chichirico@solidfire.com>
2017-09-17 16:49:27 -0600

    adding sql files for books table

---
+++ books.sql
@@ -0,0 +1,11 @@
+INSERT INTO books values ('The Philosopher''s Stone', 'J. K. Rowling', 1997);
+INSERT INTO books values ('The Chamber of Secrets', 'J. K. Rowling', 1998);
+INSERT INTO books values ('The Prisoner of Azkaban', 'J. K. Rowling', 1999);
+INSERT INTO books values ('The Goblet of Fire', 'J. K. Rowling', 2000);
+INSERT INTO books values ('The Order of the Phoenix', 'J. K. Rowling', 2003);
+INSERT INTO books values ('The Half-Blood Prince', 'J. K. Rowling', 2005);
+INSERT INTO books values ('The Deathly Hallows', 'J. K. Rowling', 2007);
+
+INSERT INTO books values ('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954);
+INSERT INTO books values ('The Two Towers', 'J.R.R. Tolkien', 1954);
+INSERT INTO books values ('The Return of the King', 'J.R.R. Tolkien', 1955);
